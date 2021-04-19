0f66bcae4aac42eda09e2e12c5835c072350274d
pdelpriore <paul.del.priore29@gmail.com>
2021-04-19 10:39:03 +0200

    add basic main view

--- src/views/app/App.tsx
+++ src/views/app/App.tsx
@@ -1,8 +1,21 @@
 import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from "react-router-dom";
+import Home from "../home/Home";
 import "./App.css";
 
 const App: React.FC = () => {
-  return <div className="App">hello</div>;
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" exact component={Home} />
+      </Switch>
+    </Router>
+  );
 };
 
 export default App;
---
+++ src/views/home/Home.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./home.css";
+
+const Home: React.FC = () => {
+  return (
+    <div className="home">
+      <div className="home__logo">logo</div>
+      <div className="home__trail">trail</div>
+      <div className="home__form">forms</div>
+      <div className="home__slogan">slogan</div>
+    </div>
+  );
+};
+
+export default Home;
