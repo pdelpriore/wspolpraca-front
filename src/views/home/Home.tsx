1f2a6cada9c2b7fe7b24cf1bd88f28e6a170f61c
pdelpriore <paul.del.priore29@gmail.com>
2021-08-22 08:33:43 +0200

    remove capitalize function

--- src/views/home/Home.tsx
+++ src/views/home/Home.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Trail from "./components/trail/Trail";
 import Signup from "./components/signup/Signup";
-import { capitalizeFirst } from "../../shared/capitalize";
 import "./home.css";
 
 const Home: React.FC = () => {
@@ -10,10 +9,10 @@ const Home: React.FC = () => {
       <div className="home__logo">logo</div>
       <div className="home__trail">
         <Trail>
-          <span>{capitalizeFirst("lorem")}</span>
-          <span>{capitalizeFirst("ipsum")}</span>
-          <span>{capitalizeFirst("dolor")}</span>
-          <span>{capitalizeFirst("sit")}</span>
+          <span>Lorem</span>
+          <span>Ipsum</span>
+          <span>Dolor</span>
+          <span>Sit</span>
         </Trail>
       </div>
       <div className="home__form">
