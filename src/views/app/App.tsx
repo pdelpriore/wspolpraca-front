bf1576659792a6739b1e3aaf587ec9663dbabe21
pdelpriore <paul.del.priore29@gmail.com>
2021-08-28 16:35:31 +0200

    add checking if user is logged or not

--- src/views/app/App.tsx
+++ src/views/app/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   BrowserRouter as Router,
   Route,
@@ -7,10 +7,17 @@ import {
 } from "react-router-dom";
 import { ApolloProvider } from "@apollo/client";
 import { client } from "../../config/apollo/client/ApolloClient";
+import { auth, onAuthChanged } from "../../config/firebase/Firebase";
 import Home from "../home/Home";
 import "./App.css";
 
 const App: React.FC = () => {
+  const [isUserLogged, setUserLogged] = useState<boolean>(false);
+
+  onAuthChanged(auth, (user) => {
+    setUserLogged(user !== null);
+  });
+
   return (
     <ApolloProvider client={client}>
       <Router>
