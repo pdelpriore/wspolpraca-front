b4f1d9e75f5d5d13a619f85a32e22776ddb2611a
pdelpriore <paul.del.priore29@gmail.com>
2021-09-11 16:33:15 +0200

    add new firebase error code

--- src/shared/firebaseAuthErrors.ts
+++ src/shared/firebaseAuthErrors.ts
@@ -5,13 +5,15 @@ export enum AuthError {
   INVALID_EMAIL = "auth/invalid-email",
   INVALID_EMAIL_VERIFIED = "auth/invalid-email-verified",
   INVALID_PASSWORD = "auth/invalid-password",
+  WEAK_PASSWORD = "auth/weak-password",
   USER_NOT_FOUND = "auth/user-not-found",
 }
 
 export const firebaseAuthError: ErrorMap<AuthError> = {
-  [AuthError.EMAIL_ALREADY_IN_USE]: "użytkownik jest już zarejestrowany",
-  [AuthError.INVALID_EMAIL]: "nieprawidłowy email",
-  [AuthError.INVALID_EMAIL_VERIFIED]: "email nie zweryfikowany",
-  [AuthError.INVALID_PASSWORD]: "nieprawidłowe hasło",
-  [AuthError.USER_NOT_FOUND]: "użytkownik nie istnieje",
+  [AuthError.EMAIL_ALREADY_IN_USE]: "Użytkownik jest już zarejestrowany",
+  [AuthError.INVALID_EMAIL]: "Nieprawidłowy email",
+  [AuthError.INVALID_EMAIL_VERIFIED]: "Email nie zweryfikowany",
+  [AuthError.INVALID_PASSWORD]: "Nieprawidłowe hasło",
+  [AuthError.WEAK_PASSWORD]: "Za słabe hasło",
+  [AuthError.USER_NOT_FOUND]: "Użytkownik nie istnieje",
 };
