ed34c3e250b732e5f5838641786e0af01a9df2e8
pdelpriore <paul.del.priore29@gmail.com>
2021-09-12 16:33:26 +0200

    implement reset password

--- src/views/home/components/forgotPassword/ForgotPassword.tsx
+++ src/views/home/components/forgotPassword/ForgotPassword.tsx
@@ -2,6 +2,14 @@ import React from "react";
 import { Card } from "react-bootstrap";
 import ForgotPasswordForm from "../form/forgotPasswordForm/ForgotPasswordForm";
 import useForm from "../../../../hooks/form/useForm";
+import useLoader from "../../../../hooks/loader/useLoader";
+import showMessage from "../../../../shared/showMessage";
+import { auth, resetPassword } from "../../../../config/firebase/Firebase";
+import {
+  AuthError,
+  firebaseAuthError,
+} from "../../../../shared/firebaseAuthErrors";
+import { FirebaseError } from "@firebase/util";
 import { TForgotPasswordInput } from "../form/forgotPasswordForm/type/forgotPasswordInputType";
 import "./forgotPassword.css";
 
@@ -13,9 +21,31 @@ const ForgotPassword: React.FC = () => {
     forgotPasswordInitInput
   );
 
-  const handleSubmitForgotPasswordForm = (e: React.FormEvent) => {
+  const [isLoading, setLoader] = useLoader<boolean>(false);
+
+  const handleSubmitForgotPasswordForm = async (e: React.FormEvent) => {
     e.preventDefault();
-    console.log("submitted");
+    try {
+      setLoader(true);
+      await resetPassword(auth, input.useremail);
+      setLoader(false);
+
+      showMessage({
+        title: "Reset hasła",
+        message: "Instrukcje dot. zmiany hasła wysłane na maila.",
+        variant: "light",
+      });
+    } catch (err) {
+      if (err instanceof FirebaseError) {
+        setLoader(false);
+
+        showMessage({
+          title: "Reset hasła",
+          message: firebaseAuthError[err.code as AuthError],
+          variant: "danger",
+        });
+      }
+    }
   };
 
   return (
@@ -27,7 +57,7 @@ const ForgotPassword: React.FC = () => {
           onChangeInput={changeForgotPasswordInput}
           onSubmitForm={handleSubmitForgotPasswordForm}
           isSubmitButtonDisabled={Object.values(input).includes("")}
-          isLoading={false}
+          isLoading={isLoading}
         />
       </Card.Body>
     </Card>
