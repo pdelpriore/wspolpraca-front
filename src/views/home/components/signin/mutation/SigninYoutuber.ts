3a19bb666db8e96f96d3ed6f4366bf29c2477438
pdelpriore <paul.del.priore29@gmail.com>
2021-09-26 22:12:48 +0200

    update signin mutation

--- src/views/home/components/signin/mutation/SigninYoutuber.ts
+++ src/views/home/components/signin/mutation/SigninYoutuber.ts
@@ -3,6 +3,7 @@ import { gql } from "@apollo/client";
 export const SIGNIN_YOUTUBER = gql`
   mutation ($signinYoutuberData: SigninUserInputType!) {
     signinYoutuber(data: $signinYoutuberData) {
+      id
       userType
       name
       email
