6137d7f3e33d14bb7be77b243456971ee099ac99
pdelpriore <paul.del.priore29@gmail.com>
2021-09-26 22:19:36 +0200

    update apollo client config

--- src/config/apollo/client/ApolloClient.ts
+++ src/config/apollo/client/ApolloClient.ts
@@ -1,6 +1,11 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client";
+
+const link = createHttpLink({
+  uri: process.env.REACT_APP_SERVER_URI,
+  credentials: "include",
+});
 
 export const client = new ApolloClient({
-  uri: process.env.REACT_APP_SERVER_URI,
   cache: new InMemoryCache(),
+  link,
 });
